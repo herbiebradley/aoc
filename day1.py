f77607e438ef6b8801ab3ee3633730caf854be42
herbiebradley <mail@herbiebradley.com>
2020-12-01 12:11:28 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,37 @@
+def part1(nums):
+    left, right = 0, len(nums) - 1
+    while True:
+        if left == right:
+            break
+        if nums[left] + nums[right] == 2020:
+            print(nums[left] * nums[right])
+            break
+        elif nums[left] + nums[right] < 2020:
+            left += 1
+        elif nums[left] + nums[right] > 2020:
+            right -= 1
+
+
+def part2(nums):
+    left, mid, right = 0, 1, len(nums) - 1
+    while True:
+        if nums[left] + nums[mid] + nums[right] == 2020:
+            print(nums[left] * nums[mid] * nums[right])
+            break
+        elif nums[left] + nums[mid] + nums[right] < 2020 and mid < right - 1:
+            mid += 1
+        elif nums[left] + nums[mid] + nums[right] < 2020 and mid == right - 1:
+            left += 1
+            mid = left + 1
+        elif nums[left] + nums[mid] + nums[right] > 2020 and mid < right - 1:
+            right -= 1
+
+
+if __name__ == "__main__":
+    nums = []
+    with open("day1.txt", 'r') as file:
+        for line in file:
+            nums.append(int(line.strip('\n')))
+    nums.sort()
+    part1(nums)
+    part2(nums)
