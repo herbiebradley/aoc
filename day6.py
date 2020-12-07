47302638277b7a16a5fe8f0953fbf7294d536e8a
herbiebradley <mail@herbiebradley.com>
2020-12-07 18:24:42 +0000

    Day 7

--- day6.py
+++ day6.py
@@ -1,4 +1,4 @@
 with open("day6.txt", 'r') as file:
     groups = [grp.split("\n") for grp in file.read().rstrip().split("\n\n")]
-print(f"Part 1: {sum([len(set(''.join(group))) for group in groups])}")
+print(f"Part 1: {sum([len(set.union(*(map(set, group)))) for group in groups])}")
 print(f"Part 2: {sum([len(set.intersection(*(map(set, group)))) for group in groups])}")
---
+++ day7.py
@@ -0,0 +1,40 @@
+import re
+
+
+def contains_gold(data, name: str) -> bool:
+    if data[name][0][0][1] == 0:
+        return False
+    elif data[name][1]:
+        return True
+    else:
+        contains: bool = False
+        for (subname, subcount) in data[name][0]:
+            if subname == "shiny gold":
+                data[name][1] = True
+                return True
+            else:
+                contains = contains or contains_gold(data, subname)
+        return contains
+
+
+def bags_inside(data, name: str) -> int:
+    if data[name][0][0][1] == 0:
+        return 0
+    else:
+        count = 0
+        for (subname, subcount) in data[name][0]:
+            count += subcount + subcount * bags_inside(data, subname)
+        return count
+
+
+with open("day7.txt", 'r') as file:
+    r = re.compile(r"[0-9]+\s[a-z]+\s[a-z]+")
+    data = {}
+    for line in file:
+        if not re.search(r"\sno\s", line):
+            data[line[:line.index("bags") - 1]] = [[(bag[2:], int(bag[:1])) for bag in re.findall(r, line)], False]
+        else:
+            data[line[:line.index("bags") - 1]] = [[('', 0)], False]
+
+print(f"Part 1: {sum([contains_gold(data, col) for col in data])}")
+print(f"Part 2: {bags_inside(data, 'shiny gold')}")
