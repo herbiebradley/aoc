876dcc78b0a19286a3b3700b897707488ef3d6dc
Herbie Bradley <mail@herbiebradley.com>
2021-12-01 10:15:47 +0000

    Day 1 2021

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,4 +1,15 @@
-if __name__ == "__main__":
-    nums = []
-    with open("day1.txt", 'r') as file:
-        for line in file:
+import numpy as np
+
+
+def part1(data):
+    return np.sum((data[1:] - data[:-1]) > 0)
+
+
+def part2(data):
+    return part1(np.sum(np.lib.stride_tricks.sliding_window_view(data, 3), axis=1))
+
+
+with open("day1.txt", 'r') as file:
+    data = np.array([int(x) for x in file.read().splitlines()])
+    print(part1(data))
+    print(part2(data))
