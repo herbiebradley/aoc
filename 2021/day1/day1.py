4bc0d010538e765dfa9d77758e901c793691b0c6
Herbie Bradley <mail@herbiebradley.com>
2021-12-01 17:55:10 +0000

    Day 1 optimisation

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -5,11 +5,7 @@ def part1(data):
     return np.sum((data[1:] - data[:-1]) > 0)
 
 
-def part2(data):
-    return part1(np.sum(np.lib.stride_tricks.sliding_window_view(data, 3), axis=1))
-
-
 with open("day1.txt", 'r') as file:
     data = np.array([int(x) for x in file.read().splitlines()])
-    print(part1(data))
-    print(part2(data))
+    print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part1(np.convolve(data, [1, 1, 1], mode='valid'))}")
