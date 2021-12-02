4c89ae4dae2280feed9bfe5094ebdce5a5f70f9b
Herbie Bradley <mail@herbiebradley.com>
2021-12-02 11:53:12 +0000

    Final opt

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -2,11 +2,11 @@ def part1(data):
     pos, depth = 0, 0
     for line in data:
         if line[0] == 'f':
-            pos += ord(line[8]) - ord('0')
+            pos += ord(line[8]) - 48
         elif line[0] == 'u':
-            depth -= ord(line[3]) - ord('0')
+            depth -= ord(line[3]) - 48
         else:
-            depth += ord(line[5]) - ord('0')
+            depth += ord(line[5]) - 48
 
     return pos * depth
 
@@ -15,13 +15,13 @@ def part2(data):
     pos, depth, aim = 0, 0, 0
     for line in data:
         if line[0] == 'f':
-            num = ord(line[8]) - ord('0')
+            num = ord(line[8]) - 48
             pos += num
             depth += aim * num
         elif line[0] == 'u':
-            aim -= ord(line[3]) - ord('0')
+            aim -= ord(line[3]) - 48
         else:
-            aim += ord(line[5]) - ord('0')
+            aim += ord(line[5]) - 48
 
     return pos * depth
 
